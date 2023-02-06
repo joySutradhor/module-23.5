1f0fcdfb776a914ab9aaaec2bd23e6b631094275
joySutradhor <joysutradhorcmt@gmail.com>
2023-02-06 16:29:01 +0600

    test

---
+++ letCost.js
@@ -0,0 +1,17 @@
+
+// ২. তুমি কি জানো কখন let দিয়ে ভেরিয়েবল লিখতে হয়ে আর কখন const দিয়ে ভেরিয়েবল লিখতে হয়। যদি লিখতে পারো তাহলে লিখে ফেলো। এবং যেই যেটা দিয়ে ভেরিয়েবল লিখলে আবার সেটা চেইঞ্জ করা যায়। তাহলে যেটা দিয়ে ভেরিয়েবল ডিক্লেয়ার করলে চেইঞ্জ করা যায়। সেটার মান চেইঞ্জ করে নাও । অর্থাৎ জাস্ট দুইটা ভেরিয়েবল ডিক্লেয়ার করবে let এবং const দিয়ে। তারপর যেকোন একটার ভ্যালু চেইঞ্জ করবে। এই let এবং const মাথা গরম করে দিলে টেনশন নিয়ে না। সামনের মাইলস্টোন এর পরের মাইলস্টোন এ এইটা নিয়ে আরো ডিসকাস করা হবে। 
+
+
+// Let format , let diye value update kora jai , kinto let diye same nam a variable assign kora jay na .
+
+let x = 10 ;
+let x = 5 ;  // error because let diye same nam a duita variable declare kora jay na .
+x = 7 ;  // correct format to update value ;
+
+
+// const format , constant ar value update kora jay na .
+
+const num = 10 ;
+const num = 10 ; // make it error
+    num = 7 ; // make it error
+
