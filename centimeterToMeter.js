3536b03afd720d5bc5ae80578dd88d970ccf32c8
joySutradhor <joysutradhorcmt@gmail.com>
2023-02-06 18:39:51 +0600

    Centimeter to meter .

---
+++ centimeterToMeter.js
@@ -0,0 +1,12 @@
+
+
+// ২. একদম ফাংশন এর নাম হুবহু centimeterToMeter নাম দিয়ে একটা ফাংশন লিখবে। এই ফাংশনে ইনপুট হিসাবে কেউ সেন্টিমিটার দিবে আর সেই সেন্টিমিটার কে মিটার এ কনভার্ট করে রেজাল্ট রিটার্ন করবে।
+
+
+function centimeterToMeter (value) {
+    let result = value / 100 ;
+    return result;
+}
+
+let centimeter = centimeterToMeter (10) ;
+console.log (centimeter);
