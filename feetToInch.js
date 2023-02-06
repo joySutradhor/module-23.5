9c8d60c17ac741d94154bfbc79cf85706f4d13eb
joySutradhor <joysutradhorcmt@gmail.com>
2023-02-06 18:33:37 +0600

    Input feet value retun to inch

---
+++ feetToInch.js
@@ -0,0 +1,12 @@
+
+
+// ১. সিম্পল একটা ফাংশন লিখতে হবে। যেটার নাম হবে feetToInch এবং এই ফাংশন ইনপুট হিসেবে নিবে feet আর রিটার্ন করবে inch । অর্থাৎ এই ফাংশনকে কোন একটা ফিট বলে দিলে সে রিটার্ন হিসেবে বলে দিবে কত ইঞ্চি হয়। 
+
+
+function feetToInch (feet) {
+    let result = feet * 12 ;
+    return result ;
+}
+
+let feet = feetToInch (10);
+console.log(feet);
